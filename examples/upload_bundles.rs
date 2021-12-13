5252d5a1af76834970d2ff26c463c644f28ce0e2
Caleb <caleb@calebeverett.io>
2021-12-13 11:32:59 -0800

    update build for commands

--- examples/upload_bundles.rs
+++ examples/upload_bundles.rs
@@ -2,10 +2,73 @@ use arloader::{commands::*, error::Error, Arweave};
 use rand::Rng;
 use rayon::prelude::*;
 use std::env;
-use std::str::FromStr;
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, str::FromStr};
 use tempdir::TempDir;
 use url::Url;
+
+// For smaller sample sizes, you may have to increase this to have the transactions mined.
+const REWARD_MULTIPLIER: f32 = 2.0;
+const NUM_FILES: usize = 20;
+const FILE_SIZE: usize = 10_000_000;
+const BUNDLE_SIZE: u64 = 100_000_000;
+const BUFFER: usize = 5;
+
+#[tokio::main]
+async fn main() -> CommandResult {
+    let ar_keypair_path = env::var("AR_KEYPAIR_PATH").ok();
+    let sol_keypair_path = env::var("SOL_KEYPAIR_PATH").ok();
+
+    let arweave = if let Some(ar_keypair_path) = ar_keypair_path {
+        Arweave::from_keypair_path(
+            PathBuf::from(ar_keypair_path),
+            Url::from_str("https://arweave.net").unwrap(),
+        )
+        .await?
+    } else {
+        if sol_keypair_path.is_none() {
+            println!("Example requires either AR_KEYPAIR_PATH or SOL_KEYPAIR_PATH environment variable to be set.");
+            return Ok(());
+        };
+        Arweave::default()
+    };
+
+    let ext = "bin";
+    let temp_dir = files_setup(FILE_SIZE, NUM_FILES, ext)?;
+    let log_dir = temp_dir.path().join("status");
+    fs::create_dir(log_dir.clone()).unwrap();
+    let glob_str = format!("{}/*.{}", temp_dir.path().display().to_string(), ext);
+    let log_dir_str = log_dir.display().to_string();
+
+    if sol_keypair_path.is_none() {
+        command_upload_bundles(
+            &arweave,
+            &glob_str,
+            Some(log_dir_str.clone()),
+            None,
+            BUNDLE_SIZE,
+            REWARD_MULTIPLIER,
+            None,
+            BUFFER,
+        )
+        .await?;
+    } else {
+        command_upload_bundles_with_sol(
+            &arweave,
+            &glob_str,
+            Some(log_dir_str.clone()),
+            None,
+            BUNDLE_SIZE,
+            REWARD_MULTIPLIER,
+            None,
+            BUFFER,
+            sol_keypair_path.as_deref().unwrap(),
+        )
+        .await?;
+    }
+
+    command_update_bundle_statuses(&arweave, &log_dir_str, None, 10).await?;
+    Ok(())
+}
 
 fn files_setup(file_size: usize, num_files: usize, ext: &str) -> Result<TempDir, Error> {
     let mut rng = rand::thread_rng();
@@ -22,43 +85,4 @@ fn files_setup(file_size: usize, num_files: usize, ext: &str) -> Result<TempDir,
         .unwrap();
     });
     Ok(temp_dir)
-}
-
-#[tokio::main]
-async fn main() -> CommandResult {
-    let ar_keypair_path = env::var("AR_KEYPAIR_PATH");
-
-    if ar_keypair_path.is_err() {
-        println!("Example requires AR_KEYPAIR_PATH environment variable to be set.");
-        return Ok(());
-    };
-    let ar_keypair_path = ar_keypair_path.unwrap();
-
-    let ext = "bin";
-    let temp_dir = files_setup(10_000_000, 20, ext)?;
-    let log_dir = temp_dir.path().join("status");
-    fs::create_dir(log_dir.clone()).unwrap();
-
-    let arweave = Arweave::from_keypair_path_sync(
-        PathBuf::from(ar_keypair_path),
-        Url::from_str("https://arweave.net/")?,
-    )?;
-
-    let glob_str = format!("{}/*.{}", temp_dir.path().display().to_string(), ext);
-    let log_dir_str = log_dir.display().to_string();
-
-    command_upload_bundles(
-        &arweave,
-        &glob_str,
-        Some(log_dir_str.clone()),
-        None,
-        100_000_000,
-        2.0,
-        None,
-        100,
-    )
-    .await?;
-
-    command_update_bundle_statuses(&arweave, &log_dir_str, None, 10).await?;
-    Ok(())
 }
